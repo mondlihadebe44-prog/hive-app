bfc25328b3f220d3f3a70b4c8f3555d2e3dd9920
mondlihadebe44-prog <mondlihadebe44@gmail.com>
2026-06-04 01:05:28 +0200

    Add files via upload

---
+++ sw (1).js
@@ -0,0 +1,20 @@
+const CACHE_NAME = 'hive-app-v3';
+
+self.addEventListener('install', event => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  // Always fetch fresh from network
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
+});
